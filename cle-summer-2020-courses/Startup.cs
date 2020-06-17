25fc962482ca6286b87177cd7030d577b1533978
Codycoder <andykohler99@gmail.com>
2020-06-17 10:59:52 -0400

    Added EFCore work

--- cle-summer-2020-courses/Startup.cs
+++ cle-summer-2020-courses/Startup.cs
@@ -27,6 +27,8 @@ namespace cle_summer_2020_courses
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<UniversityContext>();
+
             services.AddScoped<IRepository<Course>, CourseRepository>();
 
         }
--- cle-summer-2020-courses/UniversityContext.cs
+++ cle-summer-2020-courses/UniversityContext.cs
@@ -1,11 +1,52 @@
-﻿using System;
+﻿using cle_summer_2020_courses.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace cle_summer_2020_courses
 {
-    public class UniversityContext
+    public class UniversityContext : DbContext
     {
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=UniversityDB_062019;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().HasData(
+                new Course()
+                {
+                    Id = 1,
+                    Name = "Machine Learning",
+                    Description = "Machine learning for humans",
+                },
+
+                new Course()
+                {
+                    Id = 2,
+                    Name = "C# for Smarties",
+                    Description = "It's all you need to learn it all",
+                },
+
+                new Course()
+                {
+                    Id = 3,
+                    Name = "HTML, CSS, JS, oh my!",
+                    Description = "OMG, the front end will become your BFF",
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
     }
 }

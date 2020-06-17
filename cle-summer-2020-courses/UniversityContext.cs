dcd339814513315365f43d2ad5dfaf24ab748098
Codycoder <andykohler99@gmail.com>
2020-06-16 22:06:11 -0400

    first commit, added nuget packages

---
+++ cle-summer-2020-courses/UniversityContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cle_summer_2020_courses
+{
+    public class UniversityContext
+    {
+    }
+}

2c08fdcb17901d586e7fbe8254c8b8cc91749d4e
Jen E <jenevans3ten@gmail.com>
2020-06-27 16:02:41 -0400

    added new ISelectList interface with populate list method

---
+++ cle-summer-2020-courses/Repositories/ISelectList.cs
@@ -0,0 +1,13 @@
+﻿using cle_summer_2020_courses.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cle_summer_2020_courses.Repositories
+{
+    public interface ISelectList
+    {
+        List<Instructor> PopulateInstructorList();
+    }
+}

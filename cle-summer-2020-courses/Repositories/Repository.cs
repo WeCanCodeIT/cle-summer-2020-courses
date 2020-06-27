3cd34e456e25ce6b9e12a2cf3bd103dc6484a38d
Jen E <jenevans3ten@gmail.com>
2020-06-27 15:41:58 -0400

    create new generic repository with generic DbContext and T class dependencies

---
+++ cle-summer-2020-courses/Repositories/Repository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cle_summer_2020_courses.Repositories
+{
+    public class Repository<T> where T: class
+    {
+        private DbContext db;
+
+        public Repository(DbContext db)
+        {
+            this.db = db;
+        }
+
+        public void Create(T entity)
+        {
+            db.Set<T>().Add(entity);
+            db.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return db.Set<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return db.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            db.Set<T>().Update(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            db.Set<T>().Remove(entity);
+            db.SaveChanges();
+        }
+
+        public bool IsStudentAlreadyEnrolled(int courseId, int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
